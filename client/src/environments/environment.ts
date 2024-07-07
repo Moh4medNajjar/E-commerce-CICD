41c10e9874016f2459eaf48d95389aa6ffaf1a57
Moh4medNajjar <najjarmohamed443@gmail.com>
2024-07-07 17:23:41 +0100

    commit

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiEndPoint: 'http://20.250.163.2:3000/api/'
 };
 
