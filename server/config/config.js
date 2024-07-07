c604beab56e145734605e24a1e60877eb0d13861
Moh4medNajjar <134518701+Moh4medNajjar@users.noreply.github.com>
2024-07-07 15:58:12 +0100

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "MONGO_DB": process.env.MONGO_DB || "eCommerce",
-    "MONGO_IP": process.env.MONGO_IP || "127.0.0.1",
+    "MONGO_IP": process.env.MONGO_IP || "20.250.163.2",
     "MONGO_PORT": process.env.MONGO_PORT || "27017",
     "MONGO_USER": process.env.MONGO_USER || "",
     "MONGO_PASS": process.env.MONGO_PASS || "",
